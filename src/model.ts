76ffc81f1051898f7a4dadb870440d5a20235a56
frkam <gearboxop@gmail.com>
2023-06-09 23:21:09 +0300

    update tsconfig.json

--- src/model.ts
+++ src/model.ts
@@ -1,4 +1,5 @@
 import { createStore, createEvent } from 'effector';
+import { createForm } from 'effector-forms';
 import { debug } from 'patronum';
 
 export const buttonClicked = createEvent();
@@ -8,3 +9,5 @@ export const $counter = createStore(0);
 $counter.on(buttonClicked, (counter) => counter + 1);
 
 debug($counter);
+
+createForm
